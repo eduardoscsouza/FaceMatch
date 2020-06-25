76cbaaee0ea02ac3c2bcc03761b6b748f85106e1
Eduardo <eduardoscsouza@gmail.com>
2020-06-25 02:07:37 -0300

    Add triplet generator with test

--- src/data_generators.py
+++ src/data_generators.py
@@ -1,4 +1,8 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import Sequence
+import numpy as np
+import cv2
+import os
 
 
 
@@ -51,3 +55,58 @@ def get_bboxs_generator(bboxs_df, imgs_dir="../data/Img_Resize", batch_size=32,
                         validate_filenames=True)
 
     return ImageDataGenerator(**gen_args).flow_from_dataframe(bboxs_df, **flow_args)
+
+
+
+class FaceTripleGenerator(Sequence):
+    def __init__(self, indvs_df, min_indv_imgs=5, imgs_dir="../data/Img_Crop_Resize",
+                batch_size=32, out_dtype=np.float32, out_color='rgb',
+                resize=False, cv2_resize_inter=cv2.INTER_LINEAR, out_image_size=(224, 224),
+                preprocess_func=None):
+        self.indvs = indvs_df.groupby(indvs_df.columns[1])
+        self.indvs = [np.asarray(imgs.iloc[:, 0]) for _, imgs in self.indvs if len(imgs) >= min_indv_imgs]
+        self.__aux_len__ = len(self.indvs)
+        self.__aux_indvs_len__ = [len(indv) for indv in self.indvs]
+
+        self.imgs_dir = imgs_dir
+        self.batch_size = batch_size
+        self.out_dtype = out_dtype
+        self.out_color = cv2.COLOR_BGR2GRAY if (out_color == 'gray') else cv2.COLOR_BGR2RGB
+        self.resize = resize
+        self.cv2_resize_inter = cv2_resize_inter
+        self.out_image_size = out_image_size
+        self.preprocess_func = preprocess_func
+
+    def __len__(self):
+        return self.__aux_len__
+
+    def __get_random_triple__(self):
+        pos_indv, neg_indv = np.random.choice(self.__aux_len__, size=(2,), replace=False)
+        anchor, pos = np.random.choice(self.__aux_indvs_len__[pos_indv], size=(2,), replace=False)
+        neg = np.random.randint(self.__aux_indvs_len__[neg_indv])
+
+        imgs = [self.indvs[pos_indv][anchor], self.indvs[pos_indv][pos], self.indvs[neg_indv][neg]]
+        imgs = [cv2.imread(os.path.join(self.imgs_dir, img), cv2.IMREAD_COLOR) for img in imgs]
+        imgs = imgs if not self.resize else [cv2.resize(img, self.out_image_size, interpolation=self.cv2_resize_inter) for img in imgs]
+        imgs = [cv2.cvtColor(img, self.out_color) for img in imgs]
+
+        return np.stack(imgs, axis=0)
+
+    def __getitem__(self, _):
+        indvs = [self.__get_random_triple__() for _ in range(self.batch_size)]
+        indvs = np.stack(indvs, axis=0).astype(self.out_dtype)
+        indvs = [indvs[:, 0], indvs[:, 1], indvs[:, 2]]
+        indvs = indvs if self.preprocess_func is None else [self.preprocess_func(indv) for indv in indvs]
+
+        return indvs
+
+if __name__ == '__main__':
+    import pandas as pd
+    df = pd.read_csv("../data/indvs.csv")
+    gen = FaceTripleGenerator(df, imgs_dir="../data/Img_Crop_Resize")
+    it = gen.__getitem__(0)
+    for i in range(32):
+        d = os.makedirs("temp/{}".format(i), exist_ok=True)
+        cv2.imwrite("temp/{}/{}.jpg".format(i, "0"), cv2.cvtColor(it[0][i], cv2.COLOR_RGB2BGR))
+        cv2.imwrite("temp/{}/{}.jpg".format(i, "1"), cv2.cvtColor(it[1][i], cv2.COLOR_RGB2BGR))
+        cv2.imwrite("temp/{}/{}.jpg".format(i, "2"), cv2.cvtColor(it[2][i], cv2.COLOR_RGB2BGR))
